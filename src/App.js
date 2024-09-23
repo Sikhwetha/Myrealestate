95c4b84a3e1382573cd008824be44ba5995daf79
Mulanga Sikhwetha <123306977+Sikhwetha@users.noreply.github.com>
2024-09-23 11:14:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// App.jsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
@@ -7,28 +6,43 @@ import Properties from './pages/Properties/Properties';
 import Services from './pages/Services/Services';
 import Contact from './pages/Contact/Contact';
 import Navbar from './Components/Navbar/Navbar';
-import {Footer} from "./Components/Footer/Footer"
+import { Footer } from "./Components/Footer/Footer";
 import { Ourservices } from './pages/Ourservices/Ourservices';
 
 const App = () => {
-    return (
-        <>
-        <Router>
-            <div>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/properties" element={<Properties />} />
-                    <Route path="/services" element={<Services />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/ourservices" element={<Ourservices />} />
+  useEffect(() => {
+    // Create the script element for Google Ads
+    const script = document.createElement('script');
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3229999237020171";
+    script.async = true;
+    script.crossOrigin = "anonymous";
 
-                </Routes>
-            </div>
-        </Router>
-        <Footer/>
-        </>
-    );
+    // Append the script to the document head
+    document.head.appendChild(script);
+
+    // Cleanup the script when the component unmounts
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/properties" element={<Properties />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/ourservices" element={<Ourservices />} />
+          </Routes>
+        </div>
+      </Router>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
